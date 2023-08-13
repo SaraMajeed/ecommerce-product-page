2d33301164e6c528a786eecf6a063167ae299c13
Sara Majeed <smajeed434@gmail.com>
2023-08-13 20:20:47 +0100

    update image imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import CartIcon from "./CartIcon"
+import iconMenu from "../assets/images/icon-menu.svg"
+import avatar from "../assets/images/image-avatar.png";
+import logo from "../assets/images/logo.svg";
 
 export default function Header({ showMenu, toggleCart, menuItems, quantity }) {
   console.log(quantity)
   return (
     <nav className='flex flex-row sticky top-0 z-10 bg-white py-10 max-h-12 w-full items-center lg:px-32 md:px-10 lg:border-b'>
         <button className='p-5 min-w-max lg:hidden' onClick={showMenu}>
-            <img src="./src/assets/images/icon-menu.svg" alt="mobile-menu-button" />
+            <img src={iconMenu} alt="mobile-menu-button" />
         </button>
-        <img className = "mr-7" src="./src/assets/images/logo.svg" alt="logo" />
+        <img className = "mr-7" src={logo} alt="logo" />
         <div className="xs: hidden sm:hidden lg:block">
           <ul className="flex text-grayish-blue">
             {
@@ -24,7 +27,7 @@ export default function Header({ showMenu, toggleCart, menuItems, quantity }) {
             <p className={`absolute top-3 text-[10px] rounded-l-lg rounded-r-lg text-pale-orange w-6 h-4 ${quantity ? 'bg-orange' : ''}`}>{quantity? quantity : null}</p>
         </button>
         <button className='flex p-5 min-w-max min-h-max justify-end '>
-            <img className='h-12 hover:outline rounded-full outline-orange outline-2' src="./src/assets/images/image-avatar.png" alt="avatar" />
+            <img className='h-12 hover:outline rounded-full outline-orange outline-2' src={avatar} alt="avatar" />
         </button>
     </nav>
   )
--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 import { Carousel } from '@material-tailwind/react';
 import ThumbnailCarousel from './ThumbnailCarousel';
+import p1 from '../assets/images/image-product-1.jpg'
+import p2 from '../assets/images/image-product-2.jpg'
+import p3 from '../assets/images/image-product-3.jpg'
+import p4 from '../assets/images/image-product-4.jpg'
 
  
 export function ProductCarousel() {
@@ -9,22 +13,22 @@ export function ProductCarousel() {
       <div className='md:p-10 lg:hidden'>
         <Carousel className="lg:hidden z-0">
           <img
-            src="./src/assets/images/image-product-1.jpg"
+            src={p1}
             alt="image 1"
             className=""
           />
           <img
-            src="./src/assets/images/image-product-2.jpg"
+            src={p2}
             alt="image 2"
             className=""
           />
           <img
-            src="./src/assets/images/image-product-3.jpg"
+            src={p3}
             alt="image 3"
             className=""
           />
           <img
-            src="./src/assets/images/image-product-4.jpg"
+            src={p4}
             alt="image 3"
             className=""
           />
